cd19614b7abf7e2ec76a50fe65ddf4ab70702a06
mdulac <melchior.dulac@inra.fr>
2019-12-20 16:14:02 +0100

    fails to run test

--- rpToolServe.py
+++ rpToolServe.py
@@ -11,25 +11,23 @@ import io
 
 
 sys.path.insert(0, '/home/')
-import rpTool as rpGenSink
+import rpTool as rpExtractSink
 import rpToolCache
+
 
 ##############################################
 ################### REST #####################
 ##############################################
 
+
 app = Flask(__name__)
 api = Api(app)
-#dataFolder = os.path.join( os.path.dirname(__file__),  'data' )
 
-
-#TODO: test that it works well
-#declare the rpReader globally to avoid reading the pickle at every instance
 rpcache = rpToolCache.rpToolCache()
 
 
 def stamp(data, status=1):
-    appinfo = {'app': 'rpGenSink', 'version': '1.0',
+    appinfo = {'app': 'rpExtractSink', 'version': '1.0',
                'author': 'Melchior du Lac',
                'organization': 'BRS',
                'time': datetime.now().isoformat(),
@@ -55,18 +53,9 @@ class RestQuery(Resource):
     def post(self):
         inSBML = request.files['inSBML']
         params = json.load(request.files['data'])
-        #pass the cache parameters to the rpReader
-        rpgensink = rpGenSink.rpGenSink()
-        rpgensink.mnxm_strc = rpcache.mnxm_strc
-
-        #pass the files to the rpReader
-        #tf = tarfile.open(fileobj=outputTar, mode='w:xz')
-        #sink_string = rpgensink.genSink(inSBML, params['compartment_id'])
-        #sink_stringio = rpgensink.genSink(inSBML, params['compartment_id'])
-        #return send_file(sink_stringio, as_attachment=True, attachment_filename='sink.csv', mimetype='text/csv')
-        #outputSink = rpgensink.genSink(inSBML, params['compartment_id'])
-        #return send_file(io.BytesIO(outputSink.read().encode()), as_attachment=True, attachment_filename='sink.csv', mimetype='text/csv')
-        return send_file(io.BytesIO(rpgensink.genSink(inSBML, params['compartment_id']).read().encode()), as_attachment=True, attachment_filename='sink.csv', mimetype='text/csv')
+        rpextractsink = rpExtractSink.rpExtractSink()
+        rpextractsink.mnxm_strc = rpcache.mnxm_strc
+        return send_file(io.BytesIO(rpextractsink.genSink(inSBML, params['compartment_id']).read().encode()), as_attachment=True, attachment_filename='sink.csv', mimetype='text/csv')
 
 
 
