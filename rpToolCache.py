feb2474342d134c597ac3ec8d7d4796e4a268c12
mdulac <melchior.dulac@inra.fr>
2019-12-20 16:18:23 +0100

    creation of local branch

--- rpToolCache.py
+++ rpToolCache.py
@@ -69,7 +69,7 @@ class rpToolCache(rpCache):
         else:
             raise NotImplementedError('"{}" is not a valid input type'.format(itype))
         if rdmol is None:  # Check imprt
-            raise DepictionError('Import error from depiction "{}" of type "{}"'.format(idepic, itype))
+            raise self.DepictionError('Import error from depiction "{}" of type "{}"'.format(idepic, itype))
         # Export
         odepic = dict()
         for item in otype:
