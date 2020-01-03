afbf183165e9864314541e21c15e725953b6e432
mdulac <melchior.dulac@inra.fr>
2020-01-03 01:57:25 +0100

    working version

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,9 @@ FROM brsynth/rpcache
 
 COPY rpTool.py /home/
 COPY rpToolServe.py /home/
-COPY rpToolCache.py /home/
+#COPY rpToolCache.py /home/
 
-COPY test/inSBML.sbml.xml /home/
-COPY test/tool_rpExtractSink.py /home/
+#COPY test/inSBML.sbml.xml /home/
+#COPY test/tool_rpExtractSink.py /home/
 
 RUN rm -f /home/Dockerfile
-
-
-
-#ENTRYPOINT ["python"]
-#CMD ["/home/rpToolServe.py"]
-
-# Open server port
-#ONBUILD EXPOSE 8888
--- rpToolServe.py
+++ rpToolServe.py
@@ -2,10 +2,13 @@ import sys
 sys.path.insert(0, '/home/')
 
 import rpTool as rpGenSink
-import rpToolCache
+#import rpToolCache
+import rpCache
 
 def main(inSBML, compartment_id):
-    rpcache = rpToolCache.rpToolCache()
+    #rpcache = rpToolCache.rpToolCache()
+    rpcache = rpCache.rpCache()
     rpgensink = rpGenSink.rpGenSink()
     rpgensink.mnxm_strc = rpcache.mnxm_strc
-    return rpgensink.genSink(inSBML, compartment_id.read().encode())
+    with open(inSBML, 'rb') as ins:
+        return rpgensink.genSink(ins, compartment_id)
