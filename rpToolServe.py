67aaf2ad06977853862004ace11a8ced2148c029
mdulac <Melchior.DuLac@inra.fr>
2020-02-03 19:22:37 +0100

    wroking version

--- rpToolServe.py
+++ rpToolServe.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.insert(0, '/home/')
 
-import rpTool as rpGenSink
+import rpTool
 #import rpToolCache
 import rpCache
 
 def main(inSBML, compartment_id):
     #rpcache = rpToolCache.rpToolCache()
     rpcache = rpCache.rpCache()
-    rpgensink = rpGenSink.rpGenSink()
+    rpgensink = rpTool.rpExtractSink()
     rpgensink.mnxm_strc = rpcache.mnxm_strc
     with open(inSBML, 'rb') as ins:
         return rpgensink.genSink(ins, compartment_id)
